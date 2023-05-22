975a2104b9cd5e1b6a4489b7c65647e88df3bf34
gwenael9 <gwenaelgueho@gmail.com>
2023-05-22 10:59:29 +0200

    fin TP articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article", name="articles_index")
+     * @Route("/articles", name="articles_index")
      */
-    public function index() {
+    public function index(ArticleRepository $repo): Response {
         
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $repo->findAll()
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
+    public function load(ObjectManager $manager) {
+
+        for ($i=1; $i <= 10 ; $i++) { 
+            $article = new Article();
+            $article->setTitle("Article numéro : ". $i);
+            $article->setIntro("Ceci est l'intro");
+            $article->setContent("<p>Je suis du le 1er paragraphe</p>
+                                  <p>Je suis du le 2eme paragraphe</p>
+                                  <p>Je suis du le 3eme paragraphe</p>");
+            $article->setImage("https://media.cdnws.com/_i/85346/285/2264/87/blog.jpeg");
+            $article->setCreatedAt(new \DateTime());
+ 
+            $manager->persist($article);
+        }
+            
         $manager->flush();
-    }
+    } 
 }
